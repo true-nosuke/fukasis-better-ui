f307855e158f1469dbffec91903b25f5c94c5fc1
legrs <kinoko.legrs148@gmail.com>
2026-05-08 19:41:08 +0900

    fix file open problem on CALIB. add file management permission

--- app/app/src/main/java/com/example/ssa/MainActivity.java
+++ app/app/src/main/java/com/example/ssa/MainActivity.java
@@ -16,6 +16,13 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.example.ssa.databinding.ActivityMainBinding;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
@@ -64,5 +71,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        checkAllFilesAccessPermission();
+
+    }
+
+
+
+    public void checkAllFilesAccessPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Android 11 (API 30) 以上の場合
+            if (!Environment.isExternalStorageManager()) {
+                try {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                    intent.addCategory("android.intent.category.DEFAULT");
+                    intent.setData(Uri.parse(String.format("package:%s", getPackageName())));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                    startActivity(intent);
+                }
+            } else {
+                // 既に権限がある場合の処理
+                Toast.makeText(this, "ストレージフルアクセス権限があります", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Android 10以下の場合は、通常のREAD/WRITEパーミッションを要求
+            // （ここは既存のコードがあると思うので割愛します）
+        }
     }
 }
+
